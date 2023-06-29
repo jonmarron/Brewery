8877b729db92a413909dc4171a34a9be097c88c2
jonmarron <ldegjon@gmail.com>
2023-06-29 14:49:26 +0200

    Added the filter to only add one master to the list of workers and not more

--- src/main/java/com/jontxu/Brewery/logic/Brewery.java
+++ src/main/java/com/jontxu/Brewery/logic/Brewery.java
@@ -1,17 +1,19 @@
 package com.jontxu.Brewery.logic;
 
+import com.jontxu.Brewery.data.Position;
 import com.jontxu.Brewery.data.Worker;
 import com.jontxu.Brewery.data.beers.Beer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Brewery {
     private final List<Worker> workers;
     private final List<Beer> stock;
 
-    public Brewery(List<Worker> workers, List<Beer> stock) {
-        this.workers = workers;
-        this.stock = stock;
+    public Brewery() {
+        workers = new ArrayList<>();
+        stock = new ArrayList<>();
     }
 
     public boolean hasCertainAmountByName(int amount, String name){
@@ -21,5 +23,23 @@ public class Brewery {
         return count >= amount;
     }
 
+    public void addWorker(Worker worker){
+        if (worker.position().equals(Position.MASTER_BREWER) && alreadyHaveMAster()){
+            throw new IllegalArgumentException("You can't add more Master Brewers than 1.");
+        }
+        workers.add(worker);
+    }
 
+    private boolean alreadyHaveMAster() {
+        return workers.stream()
+                .anyMatch(worker -> worker.position().equals(Position.MASTER_BREWER));
+    }
+
+    public List<Beer> getStock() {
+        return stock;
+    }
+
+    public List<Worker> getWorkers() {
+        return workers;
+    }
 }
